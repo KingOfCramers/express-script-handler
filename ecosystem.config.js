d3aa620cb55d6d7f038f3ed671c8a56972749976
Harrison Cramer <harrisoncramer@gmail.com>
2020-03-08 18:13:25 -0400

    made some changes

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,14 +1,13 @@
 module.exports = {
   apps : [{
     name: 'API',
-    script: 'app.js',
-
+    script: './index.js',
     // Options reference: https://pm2.keymetrics.io/docs/usage/application-declaration/
-    args: 'one two',
-    instances: 1,
-    autorestart: true,
-    watch: false,
-    max_memory_restart: '1G',
+    // args: 'one two',
+    // instances: 1,
+    // autorestart: true,
+    // watch: false,
+    // max_memory_restart: '1G',
     env: {
       NODE_ENV: 'development'
     },
@@ -19,12 +18,13 @@ module.exports = {
 
   deploy : {
     production : {
-      user : 'node',
-      host : '212.83.163.1',
+      user : 'harrison',
+      host : '167.172.137.79',
+      key : '~/.ssh/id_rsa',
       ref  : 'origin/master',
-      repo : 'git@github.com:repo.git',
-      path : '/var/www/production',
-      'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production'
+      repo : 'git@github.com:KingOfCramers/express-script-handler.git',
+      path : '/home/harrison',
+      'post-deploy' : 'yarn install && pm2 reload ecosystem.config.js --env production && pm2 save'
     }
   }
 };
