6388b13168e6b6c837b950ca54732b29485b4cac
Harrison Cramer <harrisoncramer@gmail.com>
2020-03-19 18:53:06 -0400

    prepped for redis deploy

--- services/cache.js
+++ services/cache.js
@@ -32,18 +32,6 @@ mongoose.Query.prototype.exec = async function() {
   
   logger.info('Creating new value in cache.')
   const result = await exec.apply(this, arguments); // Execute the original mongoose search.
-  client.set(key, JSON.stringify(result)); // Turn mongoose model into string, save in redis.
+  client.set(key, JSON.stringify(result), 'EX', 10); // Turn mongoose model into string, save in redis.
   return result;
 };
-//let { minDate, maxDate } = req.query;
-//const util = require("util");
-//const redis = require("redis");
-//const redisUrl = "redis://localhost:6379";
-//let client = redis.createClient(redisUrl);
-//client.get = util.promisify(client.get);
-
-//const cachedData = await client.get(source);
-//if (cachedData){
-//console.log("SERVING FROM CACHE");
-//return res.send(cachedData);
-//client.set(source, JSON.stringify(data));
