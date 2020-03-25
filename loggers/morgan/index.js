a8c5acfdd8392d56eb936d0942453a5f5e0249a9
Harrison Cramer <harrisoncramer@gmail.com>
2020-03-25 16:04:22 -0400

    fixed silent logging on SILENT === "true" for morgan

--- loggers/morgan/index.js
+++ loggers/morgan/index.js
@@ -14,7 +14,7 @@ const errLogger = morgan((tokens,req,res) => {
   ].join(' ') 
 }, {
     skip: function (req, res) {
-      return res.statusCode < 400 || process.env.SILENT
+      return res.statusCode < 400 || process.env.SILENT === 'true'
     }, 
     stream: process.stderr
 });
@@ -32,7 +32,7 @@ const resLogger = morgan((tokens,req,res) => {
   ].join(' ') 
 }, {
     skip: function (req, res) {
-      return res.statusCode >= 400 || process.env.SILENT
+      return res.statusCode >= 400 || process.env.SILENT === 'true'
     }, 
     stream: process.stdout
 });
