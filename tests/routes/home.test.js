cee10ef653209770525bebf089eb0a20876c2c29
Harrison Cramer <harrisoncramer@gmail.com>
2020-03-26 18:09:06 -0400

    fixed home test

--- tests/routes/home.test.js
+++ tests/routes/home.test.js
@@ -4,9 +4,11 @@ require("../setup")();
 
 describe("Testing home route", () => {
   test("Successful call to home route", async done => {
-    let res = await request(app).get("/home");
-    expect(res.statusCode).toBe(200);
-    expect(res.text).toEqual('This endpoint will serve up the dashboard');
+    let res = await request(app).get("/home/");
+    expect(res.type).toBe("text/html");
+    expect(res.text).toEqual(
+      '<!doctype html><html lang="en"><head><meta charset="utf-8"><title>How to set up React, Webpack, and Babel</title></head><body><div id="app"></div><script src="bundle.js"></script></body></html>'
+    );
     done();
-  })
+  });
 });
