10a120bdb63ef0a25295277d4e3a97bcf8e53623
Harrison Cramer <harrisoncramer@gmail.com>
2020-03-30 23:42:37 -0400

    configured webpack to serve hashes

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -7,7 +7,7 @@ module.exports = {
   entry: "./src/index.js",
   output: {
     path: path.resolve(__dirname, "dist"),
-    filename: "bundle.js",
+    filename: '[name].[contenthash].js',
   },
   module: {
     rules: [
@@ -23,7 +23,7 @@ module.exports = {
   plugins: [
     new HtmlWebPackPlugin({
       template: "./src/index.html",
-      filename: "./index.html"
+      filename: "index.[contenthash].html"
     }),
     new CompressionPlugin
   ]
