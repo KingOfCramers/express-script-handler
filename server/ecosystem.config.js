fd8af60b6dd50c9e6517c4bb3dc1896aa2717d58
Harrison Cramer <harrisoncramer@gmail.com>
2020-04-04 19:42:03 -0400

    modified deploy script

--- server/ecosystem.config.js
+++ server/ecosystem.config.js
@@ -1,21 +1,17 @@
 require("dotenv").config({ path: `./envs/.${process.env.NODE_ENV}.env` });
 const path = require("path");
 
-//console.log(__dirname);
-
 module.exports = {
   apps: [
     {
-      name: "API",
-      args: [
-       "--color"
-      ],
-      interpreter: process.env.NODE_PATH, 
-      script: path.resolve(__dirname, "index.js"),
+      name: process.env.APP_NAME,
+      args: ["--color"],
+      interpreter: process.env.NODE_PATH,
+      script: path.resolve(__dirname, "server", "index.js"),
       instances: process.env.INSTANCES || 0,
       exec_mode: "cluster",
       watch: true,
-      ignore_watch: ["logs/*", "node_modules/*", "frontend/node_modules/*"],
+      ignore_watch: ["logs/*", "node_modules/*"],
       env: {
         ...process.env
       }
@@ -23,13 +19,13 @@ module.exports = {
   ],
   deploy: {
     production: {
-      user: "harrison",
+      user: 'harrison',
       host: process.env.HOST,
       key: "~/.ssh/id_rsa2",
       ref: "origin/master",
       repo: "git@github.com:KingOfCramers/express-script-handler.git",
       path: "/home/harrison/API",
-      "post-deploy": "yarn install --ignore-engines && yarn prod && cd ../client && yarn install --ignore-engines && yarn prod:build"
+      "post-deploy": `cd server && yarn install --ignore-engines && scp -r envs ${process.env.USER}@${process.env.HOST}:/home/${process.env.USER}/API/current/server && yarn prod && cd ../client && yarn install --ignore-engines && yarn prod:build`
     }
   }
 };
